cbf70a993ac37ab4c66b82bb8d67c98b3ce7f467
Onii Chan <46487694+sogood99@users.noreply.github.com>
2022-04-04 16:44:52 +0800

    Added in viewing transformations

--- DrawSVG/src/viewport.cpp
+++ DrawSVG/src/viewport.cpp
@@ -4,23 +4,24 @@
 
 namespace CMU462 {
 
-void ViewportImp::set_viewbox( float centerX, float centerY, float vspan ) {
+void ViewportImp::set_viewbox(float centerX, float centerY, float vspan) {
+    // Task 5 (part 2):
+    // Set svg coordinate to normalized device coordinate transformation. Your input
+    // arguments are defined as normalized SVG canvas coordinates.
+    this->centerX = centerX;
+    this->centerY = centerY;
+    this->vspan = vspan;
 
-  // Task 5 (part 2): 
-  // Set svg coordinate to normalized device coordinate transformation. Your input
-  // arguments are defined as normalized SVG canvas coordinates.
-  this->centerX = centerX;
-  this->centerY = centerY;
-  this->vspan = vspan; 
-
+    double inv_doub_vs = 1 / (vspan * 2);
+    double m_flat[9] = {inv_doub_vs, 0, -centerX * inv_doub_vs + 0.5, 0, inv_doub_vs, -centerY * inv_doub_vs + 0.5, 0, 0, 1};
+    set_svg_2_norm(Matrix3x3(m_flat));
 }
 
-void ViewportImp::update_viewbox( float dx, float dy, float scale ) { 
-  
-  this->centerX -= dx;
-  this->centerY -= dy;
-  this->vspan *= scale;
-  set_viewbox( centerX, centerY, vspan );
+void ViewportImp::update_viewbox(float dx, float dy, float scale) {
+    this->centerX -= dx;
+    this->centerY -= dy;
+    this->vspan *= scale;
+    set_viewbox(centerX, centerY, vspan);
 }
 
-} // namespace CMU462
+}  // namespace CMU462
